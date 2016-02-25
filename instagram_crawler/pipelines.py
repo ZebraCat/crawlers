6edd40023f78c555981269f592abf01743eb46bd
OmriManor <omri.manor@dynamicyield.com>
2016-02-25 23:38:41 +0200

    removed full name for now because of encoding issues

--- instagram_crawler/pipelines.py
+++ instagram_crawler/pipelines.py
@@ -12,10 +12,10 @@ class InstagramCrawlerPipeline(object):
     def __init__(self):
         self.conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='influencers')
         self.table = 'influencers'
-        self.COLUMNS = "inf_id, is_private, posts, username, profile_picture, full_name, followers, following, avg_comments, avg_likes"
+        self.COLUMNS = "inf_id, is_private, posts, username, profile_picture, followers, following, avg_comments, avg_likes"
 
     def process_item(self, item, spider):
         curr = self.conn.cursor()
-        curr.execute("REPLACE INTO {}({}) VALUES(%(inf_id)s, %(is_private)s, %(posts)s, %(username)s, %(profile_picture)s, %(full_name)s, "
+        curr.execute("REPLACE INTO {}({}) VALUES(%(inf_id)s, %(is_private)s, %(posts)s, %(username)s, %(profile_picture)s,"
                      "%(followers)s, %(following)s, %(avg_comments)s, %(avg_likes)s)".format(self.table, self.COLUMNS), item.__dict__['_values'])
         self.conn.commit()
--- instagram_crawler/spiders/instagram_spider.py
+++ instagram_crawler/spiders/instagram_spider.py
@@ -1,13 +1,15 @@
+import csv
 import re
 import json
-
+import logging
 from scrapy import Spider
 from instagram_crawler.items import InstagramProfileItems
 
+logger = logging.getLogger(__name__)
 
 class Instagram(Spider):
     name = "Instagram"
-    start_urls = ["http://instagram.com/nike/"]
+    start_urls = []
 
     download_delay = 0.5
 
@@ -22,7 +24,8 @@ class Instagram(Spider):
         item['following'] = data['follows']['count']
         item['followers'] = data['followed_by']['count']
         item['profile_picture'] = data['profile_pic_url']
-        item['full_name'] = data['full_name']
+        # TODO - resolve unicode problem (hebrew)
+        #item['full_name'] = data['full_name']
         item['is_private'] = data['is_private']
         media = data['media']
         item['posts'] = media['count']
@@ -34,6 +37,17 @@ class Instagram(Spider):
 
     def calc_average(self, action, media, count):
         return reduce(lambda x,y: x + y, map(lambda post: post[action]['count'], media['nodes'])) / count
+
+    def start_requests(self):
+        try:
+            with open('instagram_crawler/data/influencers.txt', 'rb') as f:
+                reader = csv.reader(f)
+                for line in reader:
+                    for name in line:
+                        yield self.make_requests_from_url("http://www.instagram.com/" + name)
+        except Exception as e:
+            logger.error("Could not parse/find influencers.txt file")
+            logger.exception(e)
 
 
 def get_extracted(value, index):
